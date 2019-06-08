3b0f8328c3f74898bb7cf21ee18aff6b70a1c6f7
Jonathan S. Machado <jonathansmachado@gmail.com>
2019-06-07 23:18:56 -0300

    Aula Flags ("g" e "i")

---
+++ executandoRegex/Flag.js
@@ -0,0 +1,9 @@
+// g - global
+// i - ignore case
+
+const text = 'Jonathan é um cara muito legal'
+
+// console.log(text.match(/J|um/))
+// console.log(text.match(/j|um/))
+// console.log(text.match(/j|um/i))
+console.log(text.match(/j|um/gi))
